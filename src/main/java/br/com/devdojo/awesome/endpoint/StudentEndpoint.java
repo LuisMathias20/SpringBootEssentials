8f17f2a05b5472b7ea265f9a3d99361f744b1cf5
LuisMathias20 <luiscraft15@gmail.com>
2022-02-11 21:48:24 -0300

    Adding the connection to the database

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,58 +1,68 @@
 package br.com.devdojo.awesome.endpoint;
 
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.devdojo.awesome.model.Student;
-import br.com.devdojo.awesome.util.DateUtil;
 import br.com.devdojo.error.CustomErrorType;
+import br.com.devdojo.repository.StudentRepository;
 
 @RestController
 @RequestMapping("students")
 public class StudentEndpoint {
-
-	private final DateUtil dateUtil;
+	
+	private final StudentRepository studentDao;
 	
 	@Autowired
-	private StudentEndpoint(DateUtil dateUtil) {
-		this.dateUtil = dateUtil;
+	private StudentEndpoint(StudentRepository studentDao) {
+		this.studentDao = studentDao;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/list")
+	@GetMapping(path = "/list")
 	public ResponseEntity<?> listAll() {
 		
-		return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+		return new ResponseEntity<>(studentDao.findAll(), HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/localDateTime")
-	public ResponseEntity<?> printLocalDateTime() {
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
 		
-		String dateTimeNow = dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now());
+		Student student = studentDao.findById(id).get();
 		
-		return new ResponseEntity<>(dateTimeNow, HttpStatus.OK);
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
-	public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
-		
-		Student student = new Student();
-		student.setId(id);
-		
-		int index = Student.studentList.indexOf(student);
-		
-		if(index == -1) {
+		if(student == null) {
 			return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
 		}
 		
-		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+		return new ResponseEntity<>(student, HttpStatus.OK);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> saveStudent(@RequestBody Student student) {		
+		return new ResponseEntity<>(studentDao.save(student), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<?> deleteStudent(@PathVariable Long id) {
+		studentDao.deleteById(id);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateStudent(@RequestBody Student student) {
+		studentDao.save(student);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
---
+++ src/main/java/br/com/devdojo/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.devdojo.awesome.model.Student;
+
+public interface StudentRepository extends CrudRepository<Student, Long>{
+	
+	List<Student> findByName(String name);
+}
