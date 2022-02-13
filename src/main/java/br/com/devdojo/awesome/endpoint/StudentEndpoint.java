71e3ec831d9efbeeb3065f65a39629082b5e77ed
LuisMathias20 <luiscraft15@gmail.com>
2022-02-13 13:32:05 -0300

    Creating more handlers and adding a paging when consulting all results

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,9 +34,9 @@ public class StudentEndpoint {
 	}
 	
 	@GetMapping(path = "/list")
-	public ResponseEntity<Iterable<Student>> listAll() {
+	public ResponseEntity<Iterable<Student>> listAll(Pageable pageable) {
 		
-		return new ResponseEntity<>(studentDao.findAll(), HttpStatus.OK);
+		return new ResponseEntity<>(studentDao.findAll(pageable), HttpStatus.OK);
 	}
 	
 	@GetMapping(path = "/{id}")
--- src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
+++ src/main/java/br/com/devdojo/handler/RestExceptionHandler.java
@@ -4,19 +4,26 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
+import br.com.devdojo.error.ErrorDetails;
 import br.com.devdojo.error.ResourceNotFoundDetails;
 import br.com.devdojo.error.ResourceNotFoundException;
 import br.com.devdojo.error.ValidationErrorDetails;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ResourceNotFoundDetails> handleResourceNotFoundException(ResourceNotFoundException rfnException) {
@@ -32,8 +39,11 @@ public class RestExceptionHandler {
 		return new ResponseEntity<>(rnfDetails, HttpStatus.NOT_FOUND);
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException manvException) {
+	@Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(	MethodArgumentNotValidException manvException, 
+																HttpHeaders headers, 
+																HttpStatus status, 
+																WebRequest request	) { 
 		List<FieldError> fieldErrors = manvException.getBindingResult().getFieldErrors();
 		
 		String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
@@ -51,5 +61,23 @@ public class RestExceptionHandler {
 													.build();
 		
 		return new ResponseEntity<>(rnfDetails, HttpStatus.BAD_REQUEST);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(	Exception ex, 
+																@Nullable Object body, 
+																HttpHeaders headers, 
+																HttpStatus status, 
+																WebRequest request	) {
+		ErrorDetails errorDetails = ResourceNotFoundDetails.Builder
+				.newBuilder()
+				.timestamp(new Date().getTime())
+				.status(status.value())
+				.title("Internal Exception")
+				.detail(ex.getMessage())
+				.developerMessage(ex.getClass().getName())
+				.build();
+		
+		return new ResponseEntity<>(errorDetails, headers, status);
 	}
 }
